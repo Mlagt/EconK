05dccf7118e55914cf55d2da823b97b31256575f
Mlagt <zetzpa@gmail.com>
2021-03-22 22:04:24 +0300

    First Version 0.0.0

---
+++ src/com/company/ReadExcel.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ReadExcel {
+
+    public static void main(String[] args) throws IOException {
+
+        // Read XSL file
+        FileInputStream inputStream = new FileInputStream(new File("D:\\project\\demo.xlsx"));
+
+        // Get the workbook instance for XLS file
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+
+        // Get first sheet from the workbook
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        // Get iterator to all the rows in current sheet
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            // Get iterator to all cells of current row
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+
+                // Change to getCellType() if using POI 4.x
+                CellType cellType = cell.getCellType();
+
+                switch (cellType) {
+                    case _NONE:
+                        System.out.print("");
+                        System.out.print("\t");
+                        break;
+                    case BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        System.out.print("\t");
+                        break;
+                    case BLANK:
+                        System.out.print("");
+                        System.out.print("\t");
+                        break;
+                    case FORMULA:
+                        // Formula
+                        System.out.print(cell.getCellFormula());
+                        System.out.print("\t");
+
+                        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+                        // Print out value evaluated by formula
+                        System.out.print(evaluator.evaluate(cell).getNumberValue());
+                        break;
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        System.out.print("\t");
+                        break;
+                    case STRING:
+                        System.out.print(cell.getStringCellValue());
+                        System.out.print("\t");
+                        break;
+                    case ERROR:
+                        System.out.print("!");
+                        System.out.print("\t");
+                        break;
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+
+}
