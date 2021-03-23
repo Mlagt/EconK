188dc0666111d303b40423337abcf088d27916ce
Anton <an.apetav@gmail.com>
2021-03-23 14:24:35 +0300

    test

--- src/com/company/FirstSection.java
+++ src/com/company/FirstSection.java
@@ -14,13 +14,13 @@ public class FirstSection {
     double incomeFromOtherCurrentActivities, incomePlanedInvest, incomePlanedFinance;
     double[] proceedsDividedCurrent = new double[6];
     //Переменные в ответе
-    double proceedsPlaned; // Выручка планируемая
-    double[] proceedsDividedPlanned = new double[6];
+    double proceedsPlaned=1; // Выручка планируемая
+    double[] proceedsDividedPlanned = new double[]{ 1, 1, 1, 1,1,1 };
 
-    double incomePlaned, incomePlanedCurrentActivities; //Доход планируемый
-    double[] incomePercentage = new double[6];
+    double incomePlaned=1, incomePlanedCurrentActivities=1; //Доход планируемый
+    double[] incomePercentage = new double[]{ 1, 1, 1, 1,1,1 };
 
-    public void firstSection(String[] args){
+    public void firstSection(){
         for (int i =0; i<6; i++){
             proceedsDividedPlanned[i] = (proceedsDividedCurrent[i] * (100 + plannedIncrease[i]))/100;
             proceedsPlaned += proceedsDividedPlanned[i];
@@ -28,8 +28,16 @@ public class FirstSection {
         incomePlanedCurrentActivities = proceedsPlaned + incomeFromOtherCurrentActivities;
         incomePlaned = incomePlanedCurrentActivities + incomePlanedInvest + incomePlanedFinance;
         for (int i =0; i<6; i++){
-            incomePercentage[i] = proceedsDividedCurrent[i]/incomePlaned;
+            incomePercentage[i] = proceedsDividedCurrent[i]/incomePlaned*100;
         }
+
+        System.out.println("proceedsPlaned= "+proceedsPlaned+"\tincomePlaned= "+incomePlaned+"\tincomePlanedCurrentActivities"+incomePlanedCurrentActivities);
+        System.out.print("proceedsDividedPlanned= ");
+        for (int i=0;i<proceedsDividedPlanned.length;i++) System.out.print(proceedsDividedPlanned[i]+" ");
+        System.out.println("");
+        System.out.print("incomePercentage= ");
+        for (int i=0;i<incomePercentage.length;i++) System.out.print(incomePercentage[i]+" ");
+
     }
 
     public void Initialization(String pathname,int variant) throws IOException {
@@ -37,19 +45,20 @@ public class FirstSection {
         FileInputStream inputStream = new FileInputStream(new File(pathname));
         XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
         for(int i=0;i<6;i++){
-           proceedsImplementation[i] = workbook.getSheetAt(0).getRow(k).getCell(variant+1).getNumericCellValue();
-           System.out.println(workbook.getSheetAt(0).getRow(k).getCell(variant+1).getNumericCellValue()); //проверка данных
+            proceedsDividedCurrent[i] = workbook.getSheetAt(0).getRow(k).getCell(variant+2).getNumericCellValue();
+           System.out.println(proceedsDividedCurrent[i]); //проверка данных
             k++;
         }
         k++;
         for(int i=0;i<6;i++){
-            plannedIncrease[i] = workbook.getSheetAt(0).getRow(k).getCell(variant+1).getNumericCellValue();
-           System.out.println(plannedIncrease[i]); // проверка данных
+            plannedIncrease[i] = workbook.getSheetAt(0).getRow(k).getCell(variant+2).getNumericCellValue();
+          System.out.println(plannedIncrease[i]); // проверка данных
             k++;
         }
-        incomePlanedInvest =  workbook.getSheetAt(0).getRow(k).getCell(variant+1).getNumericCellValue();
-        incomePlanedFinance    = workbook.getSheetAt(0).getRow(k+1).getCell(variant+1).getNumericCellValue();
-        incomeFromOtherCurrentActivities = workbook.getSheetAt(0).getRow(k+2).getCell(variant+1).getNumericCellValue();
+        incomePlanedInvest =  workbook.getSheetAt(0).getRow(k).getCell(variant+2).getNumericCellValue();
+        incomePlanedFinance    = workbook.getSheetAt(0).getRow(k+1).getCell(variant+2).getNumericCellValue();
+        incomeFromOtherCurrentActivities = workbook.getSheetAt(0).getRow(k+2).getCell(variant+2).getNumericCellValue();
         System.out.println(incomePlanedInvest+" "+incomePlanedFinance +" "+incomeFromOtherCurrentActivities);// проверка данных
+        firstSection();
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,6 @@ public class Main {
 
         FirstSection firstSection = new FirstSection();
         firstSection.Initialization(path,vatiant);
+
     }
 }
