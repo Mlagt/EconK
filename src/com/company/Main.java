910484a26bd36c5a5c770d6c6963fddeaf6353f3
Anton <an.apetav@gmail.com>
2021-03-23 12:46:14 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,13 @@ public class Main {
 
         double[] num = new double[10];
         for (int i=0;i<num.length;i++) num[i]=(int)(Math.random()*100);
-        for (int i=0;i<num.length;i++) System.out.print(num[i]+" ");
+        //for (int i=0;i<num.length;i++) System.out.print(num[i]+" ");
 
         WorkExel exel = new WorkExel();
         exel.CreatExel("Вариант 1",2,2,num);
         //for (int i=0;i<exel.number.length;i++) System.out.print(exel.number[i]+" ");
+
+        FirstSection firstSection = new FirstSection();
+        firstSection.Initialization("D:\\Java\\File\\Data.xlsx",1);
     }
 }
