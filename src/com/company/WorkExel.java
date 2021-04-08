dac33c53a41be3273fbea83857e944fe479e1ed1
Anton <an.apetav@gmail.com>
2021-04-08 12:43:25 +0300

    WorkExel

--- src/com/company/WorkExel.java
+++ src/com/company/WorkExel.java
@@ -16,69 +16,7 @@ public class WorkExel {
     public double[] number = new double[63];
     int i=0;
 
-    public void ReadExel(String pathname) throws IOException {
 
-        // Read XSL file
-        FileInputStream inputStream = new FileInputStream(new File(pathname));
-
-        // Get the workbook instance for XLS file
-        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-
-        // Get first sheet from the workbook
-        XSSFSheet sheet = workbook.getSheetAt(0);
-        // XSSFRow row = sheet.getRow(2);
-        // Get iterator to all the rows in current sheet
-        Iterator<Row> rowIterator = sheet.iterator();
-        //HSSW
-
-        while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
-            // Get iterator to all cells of current row
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-
-                // Change to getCellType() if using POI 4.x
-                CellType cellType = cell.getCellType();
-
-                switch (cellType) {
-                    case _NONE:
-                        System.out.print("");
-                        System.out.print("\t");
-                        break;
-                    case BOOLEAN:
-                        System.out.print(cell.getBooleanCellValue());
-                        System.out.print("\t");
-                        break;
-                    case FORMULA:
-                        // Formula
-                        System.out.print(cell.getCellFormula());
-                        System.out.print("\t");
-                        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
-                        // Print out value evaluated by formula
-                        System.out.print(evaluator.evaluate(cell).getNumberValue());
-                        break;
-                    case NUMERIC:
-                        System.out.print(cell.getNumericCellValue());
-                        System.out.print("\t");
-                        number[i]=cell.getNumericCellValue();
-                        i++;
-                        break;
-                    case STRING:
-                        System.out.print(cell.getStringCellValue());
-                        System.out.print("\t");
-                        break;
-                    case ERROR:
-                        System.out.print("!");
-                        System.out.print("\t");
-                        break;
-                }
-
-            }
-            System.out.println("");
-        }
-    }
     public void CreatExel(String sheetText,int rowParametr,int cellParametr,double[] cellValue) throws IOException {
         Workbook wb = new XSSFWorkbook();
         FileOutputStream fileOut = new FileOutputStream("D:\\Java\\File\\AnswerWorkbook.xlsx");
